96aab189d3ab61f0d5dd56d5f6abdbde60fa48a9
Laurent Ricci (EXT) <laurent.ricci@hotmail.com>
2023-09-04 06:23:58 +0200

    template move

--- core/memory.h
+++ core/memory.h
@@ -92,7 +92,21 @@ namespace mc68000
 		template<> uint8_t get<uint8_t>(uint32_t address)
 		{
 			uint8_t* p8 = rawMemory + (address - baseAddress);
-			return *(p8+1);
+			return *(p8 + 1);
+		}
+
+		template<> uint16_t get<uint16_t>(uint32_t address)
+		{
+			uint8_t* p8 = rawMemory + (address - baseAddress);
+			uint16_t word = (*p8++ << 8) | *p8;
+			return word;
+		}
+
+		template<> uint32_t get<uint32_t>(uint32_t address)
+		{
+			uint8_t* p8 = rawMemory + (address - baseAddress);
+			uint32_t longWord = (*p8++ << 24) | (*p8++ << 16) | (*p8++ << 8) | *p8;
+			return longWord;
 		}
 
 		uint16_t getWord(uint32_t address)
