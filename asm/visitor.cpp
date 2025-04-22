8dfb24f30d89fa25683f0bb52c426bb1efc09d7f
Laurent Ricci <lricci@microsoft.com>
2025-04-22 11:48:54 +0000

    Merged PR 129: Addressing modes
    - Adding addressing modes
    - Fixing failing Makefile

--- asm/visitor.cpp
+++ asm/visitor.cpp
@@ -35,8 +35,8 @@ std::any visitor::visitInstructionSection_abcd(parser68000::InstructionSection_a
 
 std::any visitor::visitAbcd_dRegister(parser68000::Abcd_dRegisterContext* ctx)
 {
-    uint16_t dy = std::any_cast<uint16_t>(visitDRegister(static_cast<parser68000::DRegisterContext*>(ctx->children[1])));
-    uint16_t dx = std::any_cast<uint16_t>(visitDRegister(static_cast<parser68000::DRegisterContext*>(ctx->children[3])));
+    uint16_t dy = std::any_cast<uint16_t>(visitDRegister(static_cast<parser68000::DRegisterContext*>(ctx->children[1]))) & 0b111;
+    uint16_t dx = std::any_cast<uint16_t>(visitDRegister(static_cast<parser68000::DRegisterContext*>(ctx->children[3]))) & 0b111;
 
     uint16_t opcode = 0b1100'000'10000'0'000 | (dx << 9) | dy;
     return opcode;
@@ -44,21 +44,82 @@ std::any visitor::visitAbcd_dRegister(parser68000::Abcd_dRegisterContext* ctx)
 
 std::any visitor::visitAbcd_indirect(parser68000::Abcd_indirectContext* ctx) 
 {
-    uint16_t ay = std::any_cast<uint16_t>(visitARegisterIndirectPreDecrement(static_cast<parser68000::ARegisterIndirectPreDecrementContext*>(ctx->children[1])));
-    uint16_t ax = std::any_cast<uint16_t>(visitARegisterIndirectPreDecrement(static_cast<parser68000::ARegisterIndirectPreDecrementContext*>(ctx->children[3])));
+    uint16_t ay = std::any_cast<uint16_t>(visitARegisterIndirectPreDecrement(static_cast<parser68000::ARegisterIndirectPreDecrementContext*>(ctx->children[1]))) & 0b111;
+    uint16_t ax = std::any_cast<uint16_t>(visitARegisterIndirectPreDecrement(static_cast<parser68000::ARegisterIndirectPreDecrementContext*>(ctx->children[3]))) & 0b111;
 
     uint16_t opcode = 0b1100'000'10000'1'000 | (ax << 9) | ay;
     return opcode;
 }
-
-std::any visitor::visitDRegister(parser68000::DRegisterContext* context)
+// ====================================================================================================
+// Addressing modes
+// ====================================================================================================
+std::any visitor::visitDRegister(parser68000::DRegisterContext* ctx)
 {
-    auto s = context->getText().substr(1);
-    return (uint16_t)std::stoi(s);
+    auto s = ctx->getText().substr(1);
+	auto reg = std::stoi(s);
+    return (uint16_t) (0b000'000 | reg);
+}
+
+std::any visitor::visitARegister(parser68000::ARegisterContext* ctx)
+{
+    auto s = ctx->getText().substr(1);
+    auto reg = std::stoi(s);
+    return (uint16_t)(0b001'000 | reg);
+}
+
+std::any visitor::visitARegisterIndirect(parser68000::ARegisterIndirectContext* context)
+{
+	auto s = context->children[1]->getText().substr(1);
+	auto reg = std::stoi(s);
+	return (uint16_t)(0b010'000 | reg);
+}
+
+std::any visitor::visitARegisterIndirectPostIncrement(parser68000::ARegisterIndirectPostIncrementContext* ctx)
+{
+	auto s = ctx->children[2]->getText().substr(1);
+	auto reg = std::stoi(s);
+	return (uint16_t)(0b011'000 | reg);
 }
 
 std::any visitor::visitARegisterIndirectPreDecrement(parser68000::ARegisterIndirectPreDecrementContext* ctx)
 {
     auto s = ctx->children[2]->getText().substr(1);
-    return (uint16_t)std::stoi(s);
+	auto reg = std::stoi(s);
+    return (uint16_t)(0b100'000 | reg);
 }
+
+std::any visitor::visitARegisterIndirectDisplacement(parser68000::ARegisterIndirectDisplacementContext* ctx)
+{
+	auto s = ctx->children[2]->getText().substr(1);
+	auto reg = std::stoi(s);
+	return (uint16_t)(0b101'000 | reg);
+}
+std::any visitor::visitARegisterIndirectIndex(parser68000::ARegisterIndirectIndexContext* ctx)
+{
+	auto s = ctx->children[2]->getText().substr(1);
+	auto reg = std::stoi(s);
+	return (uint16_t)(0b110'000 | reg);
+}
+std::any visitor::visitAbsoluteShort(parser68000::AbsoluteShortContext* ctx)
+{
+	return (uint16_t)(0b111'000);
+}
+std::any visitor::visitAbsoluteLong(parser68000::AbsoluteLongContext* ctx)
+{
+	return (uint16_t)(0b111'001);
+}
+std::any visitor::visitPcIndirectDisplacement(parser68000::PcIndirectDisplacementContext* ctx)
+{
+	return (uint16_t)(0b111'010);
+}
+std::any visitor::visitPcIndirectIndex(parser68000::PcIndirectIndexContext* ctx)
+{
+	return (uint16_t)(0b111'011);
+}
+std::any visitor::visitImmediateData(parser68000::ImmediateDataContext* ctx)
+{
+	return (uint16_t)(0b111'100);
+}
+
+
+
--- asm/visitor.h
+++ asm/visitor.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "parser68000BaseVisitor.h"
 #include <tree/ParseTree.h>
+
 using namespace antlr4;
 
 class visitor : public parser68000BaseVisitor
@@ -12,7 +13,18 @@ class visitor : public parser68000BaseVisitor
     std::any visitAbcd_dRegister(parser68000::Abcd_dRegisterContext* ctx);
     std::any visitAbcd_indirect(parser68000::Abcd_indirectContext* ctx);
 
+	// Addressing modes
     std::any visitDRegister(parser68000::DRegisterContext* context);
+	std::any visitARegister(parser68000::ARegisterContext* context);
+	std::any visitARegisterIndirect(parser68000::ARegisterIndirectContext* context);
+	std::any visitARegisterIndirectPostIncrement(parser68000::ARegisterIndirectPostIncrementContext* ctx);
     std::any visitARegisterIndirectPreDecrement(parser68000::ARegisterIndirectPreDecrementContext* ctx);
+	std::any visitARegisterIndirectDisplacement(parser68000::ARegisterIndirectDisplacementContext* ctx);
+	std::any visitARegisterIndirectIndex(parser68000::ARegisterIndirectIndexContext* ctx);
+	std::any visitAbsoluteShort(parser68000::AbsoluteShortContext* ctx);
+	std::any visitAbsoluteLong(parser68000::AbsoluteLongContext* ctx);
+    std::any visitPcIndirectDisplacement(parser68000::PcIndirectDisplacementContext* ctx);
+    std::any visitPcIndirectIndex(parser68000::PcIndirectIndexContext* ctx);
+    std::any visitImmediateData(parser68000::ImmediateDataContext* ctx);
 
 };
